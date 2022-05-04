f2aabeb827f880b8831a5bbb5b0b3a62b20dfb4c
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-05-04 13:05:30 +0530

    Made a program to find the distance between 2 given points

---
+++ OOPS/16_distance_between_coordinates.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <math.h>
+class co_ordinate2; // needed because at 13:45 of this program class co_ordinate1 don't know that if co_ordinate2 exists or not
+using namespace std;
+
+//-----------------CLASS 1---------------------------//
+class co_ordinate1 // made class
+{
+    int x, y; // made 2 int variables
+
+public:
+    co_ordinate1(); // made constructor
+    friend void compare(co_ordinate1 data1, co_ordinate2 data2);
+};
+co_ordinate1::co_ordinate1() //
+{
+    cout << "Enter the value of x1 : ";
+    cin >> x;
+    cout << "Enter the value of y1 : ";
+    cin >> y;
+}
+
+//-----------------CLASS 2---------------------------//
+class co_ordinate2 // made class
+{
+    int x, y; // made 2 int variables
+
+public:
+    co_ordinate2(); // made constructor
+    friend void compare(co_ordinate1 data1, co_ordinate2 data2);
+};
+co_ordinate2::co_ordinate2() //
+{
+    cout << "Enter the value of x2 : ";
+    cin >> x;
+    cout << "Enter the value of y2 : ";
+    cin >> y;
+}
+
+void compare(co_ordinate1 data1, co_ordinate2 data2) // made function which takes 2 class
+{
+    float x, y, z;
+    // x = (data2.x - data1.x) * (data2.x - data1.x); // can be also written as --> x=pow((data2.x - data1.x),2);
+    // y = (data2.y - data1.y) * (data2.y - data1.y); // can be also written as --> y=pow((data2.y - data1.y),2);
+    // further it can be also written as below :-
+    // z = sqrt(pow((data2.x - data1.x), 2) + pow((data2.x - data1.x), 2)); ----->but this was giving wrong value
+
+    // So I breaked it into small parts x and y then did manually
+    x = pow((data2.x - data1.x), 2);
+    y = pow((data2.y - data1.y), 2);
+    z = sqrt(x + y);
+    cout << "The distance between x and y co-ordinates is " << z << endl;
+}
+
+int main()
+{
+    co_ordinate1 data1;
+    co_ordinate2 data2;
+
+    compare(data1, data2); // gave compare function data1 and data2 objects
+    return 0;
+}
