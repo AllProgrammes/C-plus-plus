7b6414d900a2fcfa9b467681ac8bf17baadfac3f
mishrabiswajit <mishrabiswajit660@gmail.com>
2022-09-19 19:14:14 +0530

    Take 2 numbers and add them

---
+++ PRACTICE/01_add_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num1, num2;
+  cout << "Enter the first number : ";
+  cin >> num1;
+  cout << "Enter the second number : ";
+  cin >> num2;
+  cout << "The sum of " << num1 << " and " << num2 << " is " << num1 + num2;
+  return 0;
+}
