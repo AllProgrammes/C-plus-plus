48b9b8fab78e33851f7e5dd58ff5ea70b45b4820
mishrabiswajit <mishrabiswajit660@gmail.com>
2023-02-04 12:19:17 +0530

    Showed the use of ternary operator

---
+++ Conditionals/ternary_operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // this is just a sample program on how we use ternary operator to short our code
+  int marks = 56;
+  marks > 45 ? cout << "True" << endl : cout << "False" << endl;
+  return 0;
+}
