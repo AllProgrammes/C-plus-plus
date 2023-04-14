47c1451f1c2b2821fdafe16509add14ed146c5b4
mishrabiswajit <mishrabiswajit660@gmail.com>
2023-04-14 16:52:09 +0530

    factorial using inline fucntion + recursion

---
+++ Function/3_factorial_using_inline.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+inline int fact(int n);
+inline int fact(int n)
+{
+  if (n <= 1)
+  {
+    return 1;
+  }
+  else
+  {
+    return fact(n - 1) * n;
+  }
+}
+int main()
+{
+  int n;
+  cout << "Enter the value of n : ";
+  cin >> n;
+  cout << "Factorial of " << n << " is " << fact(n) << endl;
+  return 0;
+}
