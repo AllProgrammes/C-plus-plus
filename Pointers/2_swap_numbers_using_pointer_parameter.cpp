67e2027b0cc516dc2651ecbc0417df4428544972
mishrabiswajit <mishrabiswajit660@gmail.com>
2023-04-15 13:21:35 +0530

    No need anymore

--- Pointers/2_swap_numbers_using_pointer_parameter.cpp
+++ Pointers/2_swap_numbers_using_pointer_parameter.cpp
@@ -15,11 +15,8 @@ void swap(int *a, int *b)
 int main()
 {
   int a, b;
-  int *n1, *n2;
-  cout << "Enter the value of a : ";
-  cin >> a;
-  cout << "Enter the value of b : ";
-  cin >> b;
+  cout << "Enter the value of a and b : ";
+  cin >> a >> b;
   swap(&a, &b);
   cout << "Value of a is " << a << " and b is " << b << endl;
   return 0;
